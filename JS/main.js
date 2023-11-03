5c39bfccff8ee11b68a9b6aca098ac4600f0d7d2
Kevin E. Cruz <kevin.cruz.carcamo@gmail.com>
2023-11-02 21:39:31 -0600

    New Update

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,3 @@
-const toggleButton = document.getElementById('navbar-toggle');
-const navbarLinks = document.getElementById('navbar-links');
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-});
-
-window.addEventListener('resize', () => {
-    if (window.innerWidth > 768) {
-        navbarLinks.classList.remove('active');
-    }
+document.querySelector('.menu-toggle').addEventListener('click', function () {
+    document.querySelector('.menu').classList.toggle('open');
 });
